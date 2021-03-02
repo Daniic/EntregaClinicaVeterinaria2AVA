3a2f0abde1d50c5d0029d057fbc74f36ba6313aa
dibanyezc <dibanyezc@lmde>
2021-03-02 20:49:26 +0100

    Ultimo commit del examen

--- ClinicaVeterinaria/src/gestion/Mascota.java
+++ ClinicaVeterinaria/src/gestion/Mascota.java
@@ -1,12 +1,25 @@
 package gestion;
 
 import java.io.Serializable;
-
+/**
+ * 
+ * @author dibanyezc
+ *
+ */
 public class Mascota extends Animal implements Serializable{
 
 	private String nombre;
     private Persona duenyo;
 
+    
+    /**
+     * Constructor de la clase mascota
+     * @param ID Id de la mascota
+     * @param nombre nombre de la mascota
+     * @param edad edad de la mascota
+     * @param raza raza de la mascota
+     * @param duenyo dueño de la mascota
+     */
     public Mascota(long ID, String nombre, int edad, String raza, Persona duenyo) {
     	this.setID(ID);
         this.nombre = nombre;
@@ -15,22 +28,44 @@ public class Mascota extends Animal implements Serializable{
         this.duenyo = duenyo;
     }
 
+    /**
+     * devuelve el nombre de la mascota
+     * @return nombre de la mascota
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Setea el nombre
+     * @param nombre nuevo nombre de la mascota
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * devuelve el dueño
+     * @return dueño
+     */
+    
     public Persona getDuenyo() {
         return duenyo;
     }
 
+    /**
+     * setea el dueño de la mascota
+     * @param duenyo nuevo dueño
+     */
+    
     public void setDuenyo(Persona duenyo) {
         this.duenyo = duenyo;
     }
 
+    
+    /**
+     * permite mostrar la mascota como un string
+     */
     @Override
     public String toString() {
         return ("Nombre: " + nombre + " Edad: " + edad + " Raza: " + raza + " Due�o: " + duenyo.toString());
--- ClinicaVeterinaria/src/gestion/Persona.java
+++ ClinicaVeterinaria/src/gestion/Persona.java
@@ -1,7 +1,11 @@
 package gestion;
 
 import java.io.Serializable;
-
+/**
+ * 
+ * @author dibanyezc
+ *
+ */
 public class Persona implements Serializable{
 	private static final long serialVersionUID = 1L;
     private String nombre;
@@ -9,48 +13,101 @@ public class Persona implements Serializable{
     private String dni;
     private int edad;
 
+    /**
+     * Constructor de persona
+     * @param nombre nombre de la persona
+     * @param apellidos apellidos de la persona
+     * @param dni dni de la persona
+     * @param edad edad de la persona
+     */
     public Persona(String nombre, String apellidos, String dni, int edad) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.dni = dni;
         this.edad = edad;
     }
-
+/**
+ * devuelve el nombre de la persona
+ * @return nombre
+ */
     public String getNombre() {
         return nombre;
     }
 
+   /**
+    * setea el nombre de la persona 
+    * @param nombre nuevo nombre
+    */
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    
+    /**
+     * devuelve el apellido de la persona
+     * @return apellidos
+     */
     public String getApellidos() {
         return apellidos;
     }
 
+    /**
+     * setea el apellido de la persona
+     * @param apellidos nuevos apellidos
+     */
+    
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
+    
+    /**
+     * devuelve el dni de la persona
+     * @return dni 
+     */
     public String getDni() {
         return dni;
     }
 
+    /**
+     * setea el dni de la persona
+     * @param dni nuevo dni
+     */
+    
     public void setDni(String dni) {
         this.dni = dni;
     }
 
+    /**
+     * devuelve la edad de la persona
+     * @return edad
+     */
     public int getEdad() {
         return edad;
     }
 
+    /**
+     * setea la edad de la persona
+     * @param edad nueva edad de la persona
+     */
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
+    /**
+     * devuelve el año de nacimiento de la persona
+     * @param anyoactual año actual
+     * @return año de nacimiento de la persona
+     */
+    
     public int dameAnyoNacimiento(int anyoactual) {
         return (anyoactual - this.getEdad());
     }
+    
+    /**
+     * permite mostrar la persona como un string
+     */
     
     @Override
     public String toString() {
